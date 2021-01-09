5dfca72d80e8bcf43e85b56333b95df9ff962a47
Jose Manuel Palau Alegria <palaualegriajosemanuel@gmail.com>
2021-01-09 15:59:23 +0100

    añadidos tests

--- tests/call.test.js
+++ tests/call.test.js
@@ -1,5 +1,8 @@
-import { azertiumPrueba } from "../src/index.js";;
-
+import  azertiumPrueba  from "../src/main.js";
+//Tests no validos actualmente debido a que call llama a una promesa y el return no va a devolver otra cosa que no sea undefined
 test('call correcta', () => {
     expect(azertiumPrueba.call('framework')).toBe(true);
+  });
+test('call incorrecta', () => {
+    expect(azertiumPrueba.call('????____....')).toBe(false);
   });
